89dcd1f6a514e210be94d393fc89eaaf3471de74
Anastasiia Tymoshenko <nastyatmnk@gmail.com>
2022-10-09 16:53:21 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -7,74 +7,76 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <header className="date-search">
-          <div className="row">
-            <div className="col">
-              <ul>
-                <li>Tuesday</li>
-                <li>September 13 2022</li>
-                <li>07:14 PM</li>
-              </ul>
+      <div className="container">
+        <div className="wrapper">
+          <header className="date-search">
+            <div className="row">
+              <div className="col">
+                <ul>
+                  <li>Tuesday</li>
+                  <li>September 13 2022</li>
+                  <li>07:14 PM</li>
+                </ul>
+              </div>
+
+              <div className="col">
+                <form>
+                  <label for="search-line" className="form-lable">
+                    Change location
+                  </label>
+                  <input
+                    type="search"
+                    className="form-control form-control-sm"
+                    placeholder="Start typing..."
+                    autoComplete="off"
+                  />
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-sm"
+                    value="Search"
+                  />
+                  <button type="button" className="btn btn-warning btn-sm">
+                    Current
+                  </button>
+                </form>
+              </div>
             </div>
+          </header>
 
-            <div className="col">
-              <form>
-                <label for="search-line" className="form-lable">
-                  Change location
-                </label>
-                <input
-                  type="search"
-                  className="form-control form-control-sm"
-                  placeholder="Start typing..."
-                  autoComplete="off"
-                />
-                <input
-                  type="submit"
-                  className="btn btn-info btn-sm"
-                  value="Search"
-                />
-                <button type="button" className="btn btn-warning btn-sm">
-                  Current
-                </button>
-              </form>
-            </div>
-          </div>
-        </header>
+          <main>
+            <section>
+              <Weather />
+            </section>
 
-        <main>
-          <section>
-            <Weather />
-          </section>
+            <section>
+              <h3>5-day forecast</h3>
+              <Forecast />
+            </section>
 
-          <section>
-            <h3>5-day forecast</h3>
-            <Forecast />
-          </section>
-
-          <p className="more-details-link">
-            <a
-              href="https://www.accuweather.com/"
-              className="link-info"
-              target="_blank"
-              rel="noreferrer"
-            >
-              More details
-            </a>
-          </p>
-        </main>
+            <p className="more-details-link">
+              <a
+                href="https://www.accuweather.com/"
+                className="link-info"
+                target="_blank"
+                rel="noreferrer"
+              >
+                More details
+              </a>
+            </p>
+          </main>
+        </div>
+        <footer className="developer">
+          <a
+            href="https://github.com/Anastasiia-Tymoshenko/weather-react-app"
+            className="link-info"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          by Anastasiia Tymoshenko
+        </footer>
       </div>
-      <footer className="developer">
-        <a
-          href="https://github.com/Anastasiia-Tymoshenko/weather-react-app"
-          className="link-info"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        by Anastasiia Tymoshenko
-      </footer>
     </div>
   );
 }
